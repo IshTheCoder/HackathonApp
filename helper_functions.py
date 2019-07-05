3b5a57aef8d562a88724289931b85260abe6876d
Ishan Mehta <ishanmehta@Ishans-MacBook-Pro-3.local>
2019-07-05 17:21:59 +0530

    Push

--- helper_functions.py
+++ helper_functions.py
@@ -1,4 +1,6 @@
-def get_lineup(lineup,gameID,gamePeriod):
+
+
+def get_starting_lineup(lineup,gameID,gamePeriod):
     # Pulls the starting lineup for a given game and period from Game_Lineup.txt
     ## Inputs:
     ### lineup: game lineup data from Game_Lineup.txt
@@ -10,11 +12,15 @@ def get_lineup(lineup,gameID,gamePeriod):
     subset = lineup[(lineup['Game_id']==gameID) & (lineup['Period']==gamePeriod)]
     teams = subset.Team_id.unique().tolist()
     lineup_dict = {}
+
+    lineup_list=[]
+
     for i in range(2):
         lineup_dict[teams[i]] = subset[subset['Team_id']==teams[i]].Person_id.tolist()
-    return lineup_dict
+        lineup_list.extend(subset[subset['Team_id']==teams[i]].Person_id.tolist())
+    return lineup_list
 
-def update_lineup(lineup_dict,team,player1,player2):
+def update_lineup(lineup_list,event):
     # Substitutes in player2 for player1.
     ## Inputs:
     ### lineup_dict: dictionary with Team_id as keys and Person_id as values
@@ -23,10 +29,27 @@ def update_lineup(lineup_dict,team,player1,player2):
     ### player2: Person_id from Play_by_Play.txt
     ## Outputs:
     ### lineup_dict: dictionary with Team_id as keys and Person_id as values
-    lineup_dict[team].remove(player1)
-    lineup_dict[team].append(player2)
-    return lineup_dict
+
+    if event['Event_Msg_Type']==8:
+        #lineup_list.remove(event['Person1'])
+        lineup_list_final = list(map(lambda b: b.replace(event['Person1'],event['Person2']), lineup_list))
+
+        # print(event)
+        # print(event['Person1'])
+        # print(lineup_list)
+        # print(lineup_list_final)
+
+
+
+    #lineup_dict[team].remove(player1)
+    #lineup_dict[team].append(player2)
+        return lineup_list_final
+    else:
+        return lineup_list
+    
 
 def update_stats(stats,lineup_dict,event,action,person1,person2):
     # Uses the event and action as they pertain to person1 and person2 to update the stats according to the current lineup.
     return
+
+
--- run.py
+++ run.py
@@ -1,5 +1,3 @@
-# Use the attached data and instructions to calculate offensive rating and defensive rating for each player in each game from the 2018 Playoffs. Offensive Rating is defined as the team points scored per 100 possessions while the player is on the court. Defensive Rating is defined as the number of points per 100 possessions that the team allows while that individual player is on the court. A possession is ended by (1) made field goal attempts, (2) made final free throw attempt, (3) missed final free throw attempt that results in a defensive rebound, (4) missed field goal attempt that results in a defensive rebound, (5) turnover, or (6) end of time period. Note: When a player is substituted before or during a set of free throws but was on the court at the time of the foul that caused the free throw, he is considered to be on the court for the free throws for the purposes of offensive and defensive rating. A player substituted in before a free throw but after a foul is not considered to be on the court until after the conclusion of the free throws. Please submit a .csv file titled “Your_Team_Name_Q1_BBALL.csv” substituting in the name of your team for "Your_Team_Name". Please save as a .csv. The final product should have 4 columns. Column 1: Game_ID, Column 2: Player_ID, Column 3: OffRtg, Column 4: DefRtg. Please note that each question is permitted a maximum of two file attachments. Please submit your answer in a .csv file and save your code, spreadsheets, and all other work in a zip file.
-
 import pandas as pd
 import helper_functions as hf
 
@@ -14,11 +12,22 @@ stats['Points_made'] = stats.shape[0]*[0]
 stats['Points_given'] = stats.shape[0]*[0]
 stats['Number_possessions'] = stats.shape[0]*[0]
 
-for i in range(len(play)):
-    if game != play.Game_id[i]:
-        game = play.Game_id[i]
 
-    if period != play.Period[i]:
-        period = play.Period[i]
-        lineup = hf.get_lineup(lineup,game,period)
+
+looper=(len(play.index))
+for i in range(looper):
+
+	event=play.iloc[i,:]
+
+	#print(i)
+	if game != event['Game_id']:
+		game=event['Game_id']
+		lineup_list = hf.get_starting_lineup(lineup,game,event['Period'])
+
+
+	if period != event['Period']:
+		period = event['Period']
+    	lineup_list = hf.get_starting_lineup(lineup,game,period)
+        #print(lineup_list)
+	hf.update_lineup(lineup_list,event)
 
