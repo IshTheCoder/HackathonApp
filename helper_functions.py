78e5943a0b40c372ef45cfe627d2d4d8c393edf2
Austin Au-Yeung <austinay96@gmail.com>
2019-07-14 00:19:47 -0400

    Sort plays, revert lineup to dictionary, and track possessions

--- helper_functions.py
+++ helper_functions.py
@@ -1,4 +1,5 @@
-
+def invert(d):
+    return dict( (v,k) for k in d for v in d[k] )
 
 def get_starting_lineup(lineup,gameID,gamePeriod):
     # Pulls the starting lineup for a given game and period from Game_Lineup.txt
@@ -13,14 +14,13 @@ def get_starting_lineup(lineup,gameID,gamePeriod):
     teams = subset.Team_id.unique().tolist()
     lineup_dict = {}
 
-    lineup_list=[]
-
     for i in range(2):
         lineup_dict[teams[i]] = subset[subset['Team_id']==teams[i]].Person_id.tolist()
-        lineup_list.extend(subset[subset['Team_id']==teams[i]].Person_id.tolist())
-    return lineup_list
 
-def update_lineup(lineup_list,event):
+    lineup_dict_new=invert(lineup_dict)
+    return lineup_dict_new
+
+def update_lineup(lineup,event):
     # Substitutes in player2 for player1.
     ## Inputs:
     ### lineup_dict: dictionary with Team_id as keys and Person_id as values
@@ -32,24 +32,56 @@ def update_lineup(lineup_list,event):
 
     if event['Event_Msg_Type']==8:
         #lineup_list.remove(event['Person1'])
-        lineup_list_final = list(map(lambda b: b.replace(event['Person1'],event['Person2']), lineup_list))
+        lineup_list_final = list(map(lambda b: b.replace(event['Person1'],event['Person2']), lineup.keys()))
+        # if event['Person1'] not in lineup.keys():
+        #     print(event)
+        #     #print(lineup_dict.keys())
+
+        lineup[event['Person2']]=lineup.pop(event['Person1'])
+
+        #print(lineup_dict)
+        #print('boom')
+
+
+        #Changes required: WE HAVE TO ADD IN THE BEFORE AFTER FREE THROW CAVEAT TO THE SUBSTITUTION
+       
 
         # print(event)
         # print(event['Person1'])
         # print(lineup_list)
         # print(lineup_list_final)
 
-
-
     #lineup_dict[team].remove(player1)
     #lineup_dict[team].append(player2)
-        return lineup_list_final
+        return lineup
     else:
-        return lineup_list
+        return lineup
     
+
+
+def update_possessions(stats,lineup_dict,event,prev_team,prevPlayer):
+
+    lineup_list=lineup_dict.keys()
+    if event['Event_Msg_Type']== 1 or event['Event_Msg_Type']==5 or event['Event_Msg_Type']==3:
+
+        #Consider edge case, event code 5,0 i.e. Turnover- No turnover, what does it mean?
+        for each_player in lineup_list:
+            stats[each_player]['Possessions']+=1
+        #print(stats)
+    elif event['Event_Msg_Type']==4 and event['Action_Type']!=2:
+        #print('Rebound')
+        #print(event['Event_Num'])
+        # if lineup_dict[prevPlayer] != event['Team_id']:
+        print(event)
+        print(lineup_dict[prevPlayer]==prev_team)
+        # if lineup_dict[prevPlayer] != event['Team_id']:
+        if prev_team != event['Team_id']:
+            for each_player in lineup_list:
+                stats[each_player]['Possessions']+=1
+            #print(stats)
+
+    return 0
 
 def update_stats(stats,lineup_dict,event,action,person1,person2):
     # Uses the event and action as they pertain to person1 and person2 to update the stats according to the current lineup.
     return
-
-
--- run.py
+++ run.py
@@ -1,33 +1,53 @@
 import pandas as pd
 import helper_functions as hf
+from collections import defaultdict
 
 play = pd.read_csv("Play_by_Play.txt",delimiter="\t")
+play = play.sort_values(by=['Game_id','Event_Num'])
 lineup = pd.read_csv("Game_Lineup.txt",delimiter="\t")
 codes = pd.read_csv("Event_Codes.txt",delimiter="\t")
 
 period = 0
 game = 0
-stats = lineup[lineup['Period']==0][['Game_id','Person_id']]
-stats['Points_made'] = stats.shape[0]*[0]
-stats['Points_given'] = stats.shape[0]*[0]
-stats['Number_possessions'] = stats.shape[0]*[0]
-
-
+#stats = lineup[lineup['Period']==0][['Game_id','Person_id']]
+#stats['Points_made'] = stats.shape[0]*[0]
+#stats['Points_given'] = stats.shape[0]*[0]
+#stats['Number_possessions'] = stats.shape[0]*[0]
 
 looper=(len(play.index))
+
+team=0
+prevPlayer=0
+stats=defaultdict(lambda: defaultdict(int))
 for i in range(looper):
 
 	event=play.iloc[i,:]
 
-	#print(i)
 	if game != event['Game_id']:
 		game=event['Game_id']
-		lineup_list = hf.get_starting_lineup(lineup,game,event['Period'])
-
 
 	if period != event['Period']:
+		lineup_list = hf.get_starting_lineup(lineup,event['Game_id'],event['Period'])
 		period = event['Period']
-    	lineup_list = hf.get_starting_lineup(lineup,game,period)
-        #print(lineup_list)
-	hf.update_lineup(lineup_list,event)
 
+	hf.update_lineup(lineup_list,event)
+	# hf.update_possessions(stats,lineup_list,event,team)
+
+	if event['Event_Msg_Type']==6:
+		lineup_copy = lineup_list
+
+	if (event['Event_Msg_Type']==9) or (event['Event_Msg_Type']==8) or (event['Event_Msg_Type']==3) or (event['Event_Msg_Type']==6):
+		flag_dead = 1
+	else:
+		flag_dead = 0
+
+	if flag_dead==1:
+		hf.update_possessions(stats,lineup_copy,event,team,prevPlayer)
+	else:
+		hf.update_possessions(stats,lineup_list,event,team,prevPlayer)
+
+	team=event['Team_id']
+	prevPlayer = event['Person1']
+
+	if game != '006728e4c10e957011e1f24878e6054a':
+		break
